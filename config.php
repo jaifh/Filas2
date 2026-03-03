00621da6fbeed627d8985ee00aa7c362d5b93f44
jaifh <antoniohopperalegria@gmail.com>
2026-03-03 12:05:35 -0300

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 session_start();
 
-define('DB_HOST', 'mariadb_sistemas');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'filas_muni');
-define('DB_USER', 'user_filas');
-define('DB_PASS', '6pYyJNoMN0NR)LN/');
+define('DB_USER', 'root');
+define('DB_PASS', '198224');
 define('DB_CHARSET', 'utf8mb4');
 
 define('APP_NAME', 'Filas Permisos de Circulación');
